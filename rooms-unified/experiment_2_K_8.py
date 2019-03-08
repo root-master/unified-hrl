e1956a607fe2199e6aa06259e5732116c3ff14a8
Jacob Rafati <yrafati@gmail.com>
2019-03-07 17:06:53 -0800

    experiment for K=4

---
+++ rooms-unified/experiment_2_K_8.py
@@ -0,0 +1,45 @@
+import time
+start_time = time.time()
+
+from ExperienceReplayMemory import ExperienceReplayMemory
+experience_memory = ExperienceReplayMemory(memory_size=10000)
+
+from SubgoalDiscovery import SubgoalDiscovery
+subgoal_discovery = SubgoalDiscovery(n_clusters=8,experience_memory=experience_memory)
+
+import gym
+from gym_rooms.envs import *
+environment = 'Rooms-v0'
+env = gym.make(environment)
+ 
+from trainer import RandomWalk
+random_walk = RandomWalk(env=env,subgoal_discovery=subgoal_discovery,experience_memory=experience_memory)
+
+# lets random walk and find the subgoals such as centroids and outliers
+random_walk.walk()
+
+outliers = subgoal_discovery.outliers
+centroids = subgoal_discovery.centroid_subgoals
+subgoals = subgoal_discovery.G
+
+randomwalk_USD_time = time.time()
+print('Elapse time for unsupervised subgoal discovery: ', randomwalk_USD_time-start_time)
+
+from hrl import Controller
+controller = Controller(subgoal_discovery=subgoal_discovery)
+
+env.cross_hallway = True
+from hrl import MetaController
+meta_controller = MetaController(subgoal_discovery=subgoal_discovery)
+
+from trainer import MetaControllerControllerUnified
+meta_controller_controller_trainer = MetaControllerControllerUnified( env=env,
+									controller=controller,
+									meta_controller=meta_controller,
+									subgoal_discovery=subgoal_discovery,
+									pretrain=False)
+
+meta_controller_controller_trainer.train()
+meta_controller_controller_time = time.time() - start_time
+print('Elapsed time for training meta controller: ', meta_controller_controller_time)
+
--- rooms-unified/plotting_rooms.py
+++ rooms-unified/plotting_rooms.py
@@ -17,44 +17,6 @@ rc('legend', fontsize=12)
 results_folder = './results/'
 plots_folder = './plots/'
 
-# Experiment 1
-K = 6
-results_file_path = './results/meta_contoller_performance_results_K_' + str(K+2) + '.pkl'
-with open(results_file_path,'rb') as f:
-	A=pickle.load(f)
-	episode_rewards = A[0]
-	episode_scores = A[1]
-	episode_success = A[2]
-
-start = len(A[0])
-end = 100000+1
-
-max_episodes = 100000
-
-x_vec = list(range(0,max_episodes,200))
-x = list(range(0,max_episodes+1,1))
-# total_success_all_subgoals = []
-# for g in range(6):
-# 	results_file_path = './results/pretraining_contoller_performance_results_subgoal_' + str(g) + '.pkl'
-# 	with open(results_file_path,'rb') as f:
-# 		B=pickle.load(f)
-# 	if g == 0:
-# 		success_all_subgoals = B[0]
-# 	else:
-# 		for i in range(len(B[0])):
-# 			success_all_subgoals[i] += B[0][i]
-
-# ave_all_subgoals = []
-# for i in success_all_subgoals:
-# 	ave_all_subgoals.append(i/6.0)
-
-# results_file_path = './results/vanilla-RL_performance_results_99000.pkl'
-# with open(results_file_path,'rb') as f:
-# 	A_RL=pickle.load(f)
-# 	rewards = A_RL[0]
-# 	success = A_RL[1]
-
-
 def get_average(vector,batch=200):
 	ave = []
 	for i in range(len(vector)//batch):
@@ -66,31 +28,162 @@ def get_average(vector,batch=200):
 		ave.append(average)
 	return ave
 
-# ave_ave_all_subgoals = get_average(ave_all_subgoals)
-# success_percentage_controller = []
-# for i in ave_ave_all_subgoals:
-# 	success_percentage_controller.append(i * 100)
+# results_file_path = './results/vanilla-RL_performance_results_99000.pkl'
+# with open(results_file_path,'rb') as f:
+# 	A_RL=pickle.load(f)
+# 	rewards = A_RL[0]
+# 	success = A_RL[1]
+# episode_rewards_RL = get_average(A_RL[0],batch=198)
 
-# plt.figure()
-# plt.plot(x_vec,success_percentage_controller,'b-')
-# plt.xlabel('Training steps')
-# plt.ylabel('Success in Reaching Subgoals $\%$')
-# plot_path = plots_folder + 'rooms-controller-success.eps'
-# plt.savefig(plot_path, format='eps', dpi=1000,bbox_inches='tight')
+K = 4
+ng = K+2
+
+results_file_path = './results/meta_contoller_performance_results_K_' + str(ng) + '.pkl'
+with open(results_file_path,'rb') as f:
+	A=pickle.load(f)
+	episode_rewards = A[0]
+	episode_success = A[1]
+	# episode_success = A[2]
+start = len(A[0])
+end = 100000+1
+for i in range(start,end):
+	episode_rewards.append(50)
+	episode_success.append(1)
+
+episode_rewards = list(reversed(episode_rewards))
+episode_success = list(reversed(episode_success))
+episode_rewards_ave = get_average(episode_rewards)
+episode_rewards_ave.appen(50)
+
+
+max_episodes = 100000+1
+
+x_vec = list(range(0,max_episodes,200))
+x = list(range(0,max_episodes+1,1))
+
+total_success_all_subgoals = []
+for g in range(ng):
+	results_file_path = './results/pretraining_contoller_performance_results_K_' + str(ng) + '_subgoal_' + str(g) + '.pkl'
+	with open(results_file_path,'rb') as f:
+		B=pickle.load(f)
+	if g == 0:
+		success_all_subgoals = B[0]
+	else:
+		for i in range(len(B[0])):
+			success_all_subgoals[i] += B[0][i]
+
+ave_all_subgoals = []
+for i in success_all_subgoals:
+	ave_all_subgoals.append(i/float(ng))
+
+ave_ave_all_subgoals = get_average(ave_all_subgoals)
+success_percentage_controller = []
+for i in ave_ave_all_subgoals:
+	success_percentage_controller.append(i * 100)
+
+success_percentage_controller_K4 = success_percentage_controller
+
+
+K = 6
+ng = K+2
+
+results_file_path = './results/meta_contoller_performance_results_K_' + str(ng) + '.pkl'
+with open(results_file_path,'rb') as f:
+	A=pickle.load(f)
+	episode_rewards = A[0]
+	episode_scores = A[1]
+	episode_success = A[2]
+
+start = len(A[0])
+end = 100000+1
+max_episodes = 100000
+
+x_vec = list(range(0,max_episodes,200))
+x = list(range(0,max_episodes+1,1))
+
+total_success_all_subgoals = []
+for g in range(ng):
+	results_file_path = './results/pretraining_contoller_performance_results_K_' + str(ng) + '_subgoal_' + str(g) + '.pkl'
+	with open(results_file_path,'rb') as f:
+		B=pickle.load(f)
+	if g == 0:
+		success_all_subgoals = B
+	else:
+		for i in range(len(B)):
+			success_all_subgoals[i] += B[i]
+
+ave_all_subgoals = []
+for i in success_all_subgoals:
+	ave_all_subgoals.append(i/float(ng))
+
+ave_ave_all_subgoals = get_average(ave_all_subgoals)
+success_percentage_controller = []
+for i in ave_ave_all_subgoals:
+	success_percentage_controller.append(i * 100)
+
+success_percentage_controller_K6 = success_percentage_controller
+###################################################################
+K = 8
+ng = K+2
+results_file_path = './results/meta_contoller_performance_results_K_' + str(ng) + '.pkl'
+with open(results_file_path,'rb') as f:
+	A=pickle.load(f)
+	episode_rewards = A[0]
+	episode_scores = A[1]
+	episode_success = A[2]
+
+max_episodes = 100000
+
+x_vec = list(range(0,max_episodes,200))
+x = list(range(0,max_episodes+1,1))
+
+total_success_all_subgoals = []
+for g in range(ng):
+	results_file_path = './results/pretraining_contoller_performance_results_K_' + str(ng) + '_subgoal_' + str(g) + '.pkl'
+	with open(results_file_path,'rb') as f:
+		B=pickle.load(f)
+	if g == 0:
+		success_all_subgoals = B
+	else:
+		for i in range(len(B)):
+			success_all_subgoals[i] += B[i]
+
+ave_all_subgoals = []
+for i in success_all_subgoals:
+	ave_all_subgoals.append(i/float(ng))
+
+ave_ave_all_subgoals = get_average(ave_all_subgoals)
+success_percentage_controller = []
+for i in ave_ave_all_subgoals:
+	success_percentage_controller.append(i * 100)
+
+success_percentage_controller_K8 = success_percentage_controller
+############################################################################
+
+plt.figure()
+plt.plot(x_vec,success_percentage_controller_K4,'b-',label='$K=4$')
+plt.plot(x_vec,success_percentage_controller_K6,'r-',label='$K=6$')
+plt.plot(x_vec,success_percentage_controller_K8,'g-',label='$K=8$')
+
+plt.xlabel('Training steps')
+plt.ylabel('Success in Reaching Subgoals $\%$')
+plot_path = plots_folder + 'rooms-controller-success.eps'
+plt.legend(loc=0)
+plt.savefig(plot_path, format='eps', dpi=1000,bbox_inches='tight')
 
 episode_rewards_ave = get_average(episode_rewards)
 # episode_rewards_RL = get_average(A_RL[0],batch=198)
-plt.figure()
+# plt.figure()
 # plt.plot(x_vec,episode_rewards_ave,'b-',label='Our Unified Model-Free HRL Method')
-plt.plot(x,episode_rewards,'b-',label='Our Unified Model-Free HRL Method')
+# plt.plot(x,episode_rewards,'b-',label='Our Unified Model-Free HRL Method')
 
 # plt.plot(x_vec,episode_rewards_RL,'r-',label='Regular RL')
-plt.xlabel('Training steps')
-plt.ylabel('Episode Return')
-plt.legend(loc=0)
+# plt.xlabel('Training steps')
+# plt.ylabel('Episode Return')
+# plt.legend(loc=0)
 # plot_path = plots_folder + 'rooms-success.eps'
 # plt.savefig(plot_path, format='eps', dpi=1000,bbox_inches='tight')
-plt.show()
+# plt.show()
 # episode_success_RL = get_average(A_RL[1],batch=198)
 # plt.figure()
 # episode_success_ave = get_average(episode_success)
